2059de50c83b17b4afd95d579e3718c3848de87d
Lucas Wang <lucaschiwang@gmail.com>
2017-04-10 00:47:20 +1000

    section 7.3 done

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var watchify = require('watchify');
 
 gulp.task('bundle', () => {
 	return browserify('src/App.js')
@@ -8,4 +9,25 @@ gulp.task('bundle', () => {
 		.bundle()
 		.pipe(source('bundle.js'))
 		.pipe(gulp.dest('static/'));
+});
+
+gulp.task('watch', () => {
+	var b = browserify({
+		entries: ['src/App.js'],
+		cache: {}, packageCache: {},
+		plugin: ['watchify']
+	});
+
+	b.on('update', makeBundle);
+
+	function makeBundle() {
+		b.transform('babelify', { presets: 'react' })
+			.bundle()
+			.pipe(source('bundle.js'))
+			.pipe(gulp.dest('static/'));
+	};
+
+	makeBundle();
+
+	return b;
 });
